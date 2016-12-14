daf49224024828dc3bd4151ac1b90828d6f9326b
alex-bebyakov <alex0181@yandex.ru>
2016-12-14 14:49:06 +0300

    Update dixit.js

--- dixit.js
+++ dixit.js
@@ -1,7 +1,13 @@
-const express = require('express');
-const app = express();
-const server = app.listen(5002);
-const io = require('socket.io')(server);
+var express = require("express");
+var app = express();
+app.use(express.logger());
+var port = process.env.PORT || 5002;
+
+var server = app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
+var io = require('socket.io')(server);
 
 require('./server/config/express')(app,__dirname);
 require('./server/services/socket.service')(io);
